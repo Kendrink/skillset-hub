ae0c674baa803d9db7fda2cc601bbd6c2ab60aa5
Kendrink <you@example.com>
2024-03-13 23:04:51 -0700

    edited and modified package.json

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,5 +1,5 @@
 const isLocalhost = Boolean(
-  window.location.hostname === "localhost" ||
+  window.location.hostname === "localhost" || 3001,
     window.location.hostname === "[::1]" ||
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
